cbf04421cd1da5ce225b9cd530e5e290ea036d00
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-05-16 15:45:04 -0300

    Cria testes para o endpoint matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -13,6 +13,7 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 process.env.JWT_SECRET = 'jwt_secret';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoidXNlckB1c2VyLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X3VzZXIifSwiaWF0IjoxNjgzOTI5MDA1LCJleHAiOjE2ODQ1MzM4MDV9.yxv3DhfPostdbkTUqzZleRjhCkGLi7lXgXo5oYgom7E';
 
 describe('Matches', () => {
   /**
@@ -42,7 +43,6 @@ describe('Matches', () => {
   // });
 
   it('Testa se matches funciona', async () => {
-    //expect(false).to.be.eq(false);
     const res = await chai
     .request(app)
     .get('/matches');
@@ -51,7 +51,6 @@ describe('Matches', () => {
   });
 
   it('Testa se as queries funcionam', async () => {
-    //expect(false).to.be.eq(false);
     const res1 = await chai
     .request(app)
     .get('/matches?inProgress=false');
@@ -63,4 +62,68 @@ describe('Matches', () => {
     expect(res2.body[0].awayTeamId).to.eq(9);
   });
 
+  it('Testa se a criação de partidas funciona', async () => {
+    //Testa se uma partida válida é adicionada
+    const match1 = {
+      "homeTeamId": 2, 
+      "awayTeamId": 3,
+      "homeTeamGoals": 2,
+      "awayTeamGoals": 1
+    };
+    const res1 = await chai
+    .request(app)
+    .post('/matches')
+    .set('authorization', token)
+    .send(match1);
+    expect(res1.body.homeTeamId).to.eq(2);
+
+    //Testa se uma partida com o mesmo time duas vezes dá erro
+    const match2 = {
+      "homeTeamId": 2, 
+      "awayTeamId": 2,
+      "homeTeamGoals": 2,
+      "awayTeamGoals": 1
+    };
+    const res2 = await chai
+    .request(app)
+    .post('/matches')
+    .set('authorization', token)
+    .send(match2);
+    expect(res2.status).to.eq(422);
+
+    //Testa se uma partida com um time invalido dá erro
+    const match3 = {
+      "homeTeamId": -1, 
+      "awayTeamId": 2,
+      "homeTeamGoals": 2,
+      "awayTeamGoals": 1
+    };
+    const res3 = await chai
+    .request(app)
+    .post('/matches')
+    .set('authorization', token)
+    .send(match3);
+    expect(res3.status).to.eq(404);
+  });
+
+  it('Testa se a modificação de partidas funciona', async () => {
+    const match = {
+      "homeTeamGoals": 3,
+      "awayTeamGoals": 1
+    };
+    const res = await chai
+    .request(app)
+    .patch('/matches/1')
+    .set('authorization', token)
+    .send(match);
+    expect(res.status).to.eq(200);
+  });
+
+  it('Testa se a finalização de partidas funciona', async () => {
+    const res = await chai
+    .request(app)
+    .patch('/matches/1/finish')
+    .set('authorization', token);
+    expect(res.status).to.eq(200);
+  });
 });
